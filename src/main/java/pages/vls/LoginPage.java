8a3569e655fb67bac58e02f02328c7618d31ac6d
ahmed.afify <6hdlhzeuh62pog2o6nsafjkad23sbzccumobugtyjcsgag543uaa>
2022-02-09 09:10:19 +0400

    Update for Vehicle Renewal 2

--- src/main/java/pages/vls/LoginPage.java
+++ src/main/java/pages/vls/LoginPage.java
@@ -2,16 +2,30 @@ package pages.vls;
 
 import com.shaft.gui.element.ElementActions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
     private WebDriver driver ;
     public LoginPage(WebDriver driver){this.driver=driver;}
     By eidTxt = By.id("idNationalId");
     By loginBtn = By.xpath("//button[text()='Login']");
+    By loadingBall= By.xpath("//div[@class='double-bounce1']");
+    By loadingBall2= By.xpath("//div[@class='double-bounce2']");
 
     public void login(String EID){
         ElementActions.type(driver,eidTxt, EID);
         ElementActions.click(driver,loginBtn);
     }
+
+    public void bounceWait(){
+        new WebDriverWait(driver, 10).until(ExpectedConditions.invisibilityOfElementLocated(loadingBall2));
+    }
+
 }
+
+
