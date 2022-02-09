548cbdee7d984b81f7ee744aa5a447cd8eb997c3
ahmed.afify <6hdlhzeuh62pog2o6nsafjkad23sbzccumobugtyjcsgag543uaa>
2022-02-09 17:24:48 +0400

    Update for Vehicle Renewal 3

--- src/main/java/pages/vls/LoginPage.java
+++ src/main/java/pages/vls/LoginPage.java
@@ -14,17 +14,15 @@ public class LoginPage {
     public LoginPage(WebDriver driver){this.driver=driver;}
     By eidTxt = By.id("idNationalId");
     By loginBtn = By.xpath("//button[text()='Login']");
-    By loadingBall= By.xpath("//div[@class='double-bounce1']");
-    By loadingBall2= By.xpath("//div[@class='double-bounce2']");
+
+
 
     public void login(String EID){
         ElementActions.type(driver,eidTxt, EID);
         ElementActions.click(driver,loginBtn);
     }
 
-    public void bounceWait(){
-        new WebDriverWait(driver, 10).until(ExpectedConditions.invisibilityOfElementLocated(loadingBall2));
-    }
+
 
 }
 
