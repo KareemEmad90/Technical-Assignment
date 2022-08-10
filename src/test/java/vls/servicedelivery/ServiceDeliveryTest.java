c677d99be43a00f932619c6b482e6af75709d966
mohamed_abdo <mohamed.abdo@isoft.ae>
2022-08-10 09:55:07 +0400

    Changes related to vehicle renewal

--- src/test/java/vls/servicedelivery/ServiceDeliveryTest.java
+++ src/test/java/vls/servicedelivery/ServiceDeliveryTest.java
@@ -1,6 +1,5 @@
 package vls.servicedelivery;
 
-import api.AddElectronicInsurance;
 import com.shaft.gui.browser.BrowserActions;
 import com.shaft.gui.browser.BrowserFactory;
 import data.DbQueries;
@@ -8,16 +7,14 @@ import data.LoadProperties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.Dashboard.Corporate.CorporateDashboardPage;
 import pages.Individual.IndividualDashboardPage;
 import pages.common.ChromeCertificatePage;
+import pages.serviceDelivery.CourierDeliveryAddressPage;
 import pages.serviceDelivery.CourierDeliveryTypesPage;
 import pages.vls.VehicleDetailsPage;
 import pages.vls.VehiclesPage;
-import pages.vls.publicVehicleRenewal.VehicleInformation;
-import pages.vls.publicVehicleRenewal.VehicleNumberPlate;
 
 import java.sql.SQLException;
 
@@ -41,9 +38,9 @@ public class ServiceDeliveryTest {
 
         DbQueries dbQueries = new DbQueries();
         String[] vehicle = dbQueries.getVehiclesReadyForRenewal();
-        String ChassisNo = "19XFB2642EE900908";//vehicle[0];
-        String TrafficCodeNumber = "10108442";//vehicle[1];
-        String  PlateNumber= vehicle[2];
+        String ChassisNo = "1C4BJWAG3GL281207";//vehicle[0];
+        String TrafficCodeNumber = "10343518";//vehicle[1];
+        /*String  PlateNumber= vehicle[2];
         String  PlateCode = vehicle[3];
         String  MobileNumber = vehicle[4];
         AddElectronicInsurance insurance= new AddElectronicInsurance();
@@ -51,6 +48,8 @@ public class ServiceDeliveryTest {
         dbQueries.addTest(ChassisNo);
         dbQueries.resetviloation(TrafficCodeNumber, ChassisNo);
         dbQueries.removeBlocker(TrafficCodeNumber);
+
+         */
         corporateDashboardPage.sddiLogin(TrafficCodeNumber);
         individualDashboardPage.clickVehiclesBox();
         vehiclesPage = new VehiclesPage(driver);
@@ -58,23 +57,20 @@ public class ServiceDeliveryTest {
         vehiclesPage.selectVehicle();
         vehicleDetailsPage = new VehicleDetailsPage(driver);
         vehicleDetailsPage.selectService("renew");
-        //vehicleDetailsPage.cancelJourney();
-        VehicleInformation vehicleInformation = new VehicleInformation(driver);
+        vehicleDetailsPage.resumeJourney();
+       /* VehicleInformation vehicleInformation = new VehicleInformation(driver);
         vehicleInformation.vehicleInformationPage();
         VehicleNumberPlate vehicleNumberPlate = new VehicleNumberPlate(driver);
         vehicleNumberPlate.vehicleNumberPlatepage();
-
+*/
         courierDeliveryTypesPage = new CourierDeliveryTypesPage(driver);
     }
     @Test
     public void premiumDeliveryTest() throws InterruptedException {
-        DbQueries dbQueries = new DbQueries();
-        String[] vehicle = dbQueries.getVehiclesReadyForRenewal();
-        chassisNo =vehicle[0];
 
         courierDeliveryTypesPage.selectDeliveryType("PREMIUM");
-        /*ReviewAndPayment reviewPayment = new ReviewAndPayment(driver);
-        reviewPayment.reviewAndPaymentPage();*/
+
+
     }
     @Test
     public void sameDayDeliveryTest(){
@@ -86,6 +82,16 @@ public class ServiceDeliveryTest {
     @Test
     public void standardDeliveryTest(){
         courierDeliveryTypesPage.selectDeliveryType("STANDARD");
+        CourierDeliveryAddressPage courierDeliveryAddressPage = new CourierDeliveryAddressPage(driver);
+        //courierDeliveryAddressPage.fillApartmentAddress();
+        courierDeliveryAddressPage.fillVillaAddress();
+        courierDeliveryAddressPage.selectCity("Dubai");
+        courierDeliveryAddressPage.selectArea("Abu Hail ( Deira )");
+        courierDeliveryAddressPage.markPreferredAddress();
+        courierDeliveryAddressPage.fillContactInfo("505155849","2505155849");
+        courierDeliveryAddressPage.selectDate("15");
+        courierDeliveryAddressPage.checkTermsAndConditions();
+        courierDeliveryAddressPage.clickOnSaveResume();
     }
 
     @Test
@@ -103,7 +109,7 @@ public class ServiceDeliveryTest {
 
     @AfterMethod
     public void afterMethod() {
-        BrowserActions.closeCurrentWindow(driver);
+      //  BrowserActions.closeCurrentWindow(driver);
     }
 
 }
