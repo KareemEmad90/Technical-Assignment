3d74e64fbac28fd226d036fdc63ad9807428ac55
Kareem Emad <kareem_emad90@yahoo.com>
2022-09-05 03:19:31 +0400

    Technical assignment

--- src/test/java/AddProduct/AddProductTest.java
+++ src/test/java/AddProduct/AddProductTest.java
@@ -25,7 +25,6 @@ public class AddProductTest {
         driver = BrowserFactory.getBrowser();
         BrowserActions.navigateToURL(driver, LoadProperties.userData.getProperty("openWebSite"));
 
-
     }
 
 
